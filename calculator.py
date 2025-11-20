86cac4b50c4409438fa221924a4e1ab828f2e014
Gengyuan Zhang <gengyuanzhang@Gengyuans-MacBook-Pro.local>
2025-11-19 19:28:07 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -7,18 +7,18 @@ One function per operation, in order.
 import math
 def add(a,b):
     return a+b
-def sub(a,b):
+def subtract(a,b):
     return a-b
-def mul(a,b):
+def multiply(a,b):
     return a*b
-def div(a,b):
+def divide(a,b):
     if a==0:
         raise ZeroDivisionError
     return b/a
-def log(a,b):
+def logarithm(a,b):
     if not (a>0 and a!=1 and b>0):
         raise ValueError
     return math.log(b)/math.log(a)
-def exp(a, b):
+def exponent(a, b):
     return a**b
 
